6de5198e63ef114e0908fb48b7e398377f288e29
muhammadabrar <abrarmuhammad100@gmail.com>
2021-08-15 15:58:51 +0500

    checking prototype

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <h1>warka dang</h1>
+      <div className="App">
+        {/* <Navbar /> */}
+        <div className="content">
+          {/* <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/categories/:category">
+              <Categories />
+            </Route>
+            <Route path="/product/:id">
+              <Product />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch> */}
+        </div>
+      </div>
+    </Router>
   );
 }
 
